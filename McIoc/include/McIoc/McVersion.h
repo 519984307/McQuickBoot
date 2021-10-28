d49995c92dc885a5dd4787c42d0df2e88ac3ad24
mrcao20 <839566521@qq.com>
2021-10-28 21:12:01 +0800

    feat: v1.6.0 init

--- McIoc/include/McIoc/McVersion.h
+++ McIoc/include/McIoc/McVersion.h
@@ -29,11 +29,11 @@
 //! version when you make big feature changes.
 #define MC_MAJOR 1
 //! version when you make small feature changes.
-#define MC_MINOR 5
+#define MC_MINOR 6
 //! version when you make backwards-compatible bug fixes.
-#define MC_PATCH 3
+#define MC_PATCH 0
 //! MC_INTERNAL version for perpurse like feature test, bug fix test,development, et
-#define MC_INTERNAL 8
+#define MC_INTERNAL 0
 
 #ifdef QT_DEBUG
 #define MC_VERSION_STR (QString::number(MC_MAJOR) + "." + QString::number(MC_MINOR) + "." + QString::number(MC_PATCH) + "." + QString::number(MC_INTERNAL))
